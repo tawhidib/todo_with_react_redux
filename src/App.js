16eeda9cc3cfc384e4c8a15c6521d9edb1e22203
tawhidib <tawhidib@gmail.com>
2023-02-27 13:13:57 +0600

    pref: Footer component created and saperated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
@@ -19,21 +20,7 @@ function App() {
         <hr class="mt-4" />
 
         {/* <!-- footer --> */}
-        <div class="mt-4 flex justify-between text-xs text-gray-500">
-          <p>2 tasks left</p>
-          <ul class="flex space-x-1 items-center text-xs">
-            <li class="cursor-pointer font-bold">All</li>
-            <li>|</li>
-            <li class="cursor-pointer">Incomplete</li>
-            <li>|</li>
-            <li class="cursor-pointer">Complete</li>
-            <li></li>
-            <li></li>
-            <li class="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
-            <li class="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
-            <li class="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
-          </ul>
-        </div>
+        <Footer />
       </div>
     </div>
   );
