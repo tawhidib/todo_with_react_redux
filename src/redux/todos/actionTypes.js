75d880fd720c5826f049aebe7b5f79301ab438d4
tawhidib <tawhidib@gmail.com>
2023-02-27 13:55:34 +0600

    feat:(actionTypes): todos action-type created

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = "todo/added";
+export const ALLCOMPLETED = "todo/allcompleted";
+export const CLEARCOMPLETED = "todo/clearcompleted";
+export const TOGGLED = "todo/toggled";
+export const DELETED = "todo/deleted";
+export const COLORSELECTED = "todo/colorselected";
