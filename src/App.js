f18ad24da729f362a22d7e18b2f1de0e64f6c59e
tawhidib <tawhidib@gmail.com>
2023-02-27 13:10:49 +0600

    pref: Header component created and saperated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,34 +7,9 @@ function App() {
     <div class="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
       {/* <!-- navbar --> */}
       <Navbar />
-      {/* <!-- header --> */}
       <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-        <div>
-          <form class="flex items-center bg-gray-100 px-4 py-4 rounded-md">
-            <img src="./images/notes.png" class="w-6 h-6" alt="Add todo" />
-            <input
-              type="text"
-              placeholder="Type your todo"
-              class="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-            />
-            <button
-              type="submit"
-              class="appearance-none w-8 h-8 bg-[url('./images/plus.png')] bg-no-repeat bg-contain"
-            ></button>
-          </form>
-
-          <ul class="flex justify-between my-4 text-xs text-gray-500">
-            <li class="flex space-x-1 cursor-pointer">
-              <img
-                class="w-4 h-4"
-                src="./images/double-tick.png"
-                alt="Complete"
-              />
-              <span>Complete All Tasks</span>
-            </li>
-            <li class="cursor-pointer">Clear completed</li>
-          </ul>
-        </div>
+        {/* <!-- header --> */}
+        <Header />
         <hr class="mt-4" />
 
         {/* <!-- todo list --> */}
