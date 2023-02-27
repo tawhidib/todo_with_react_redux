cb459415ac2005f0fbf48cedf1c314c74b26ac69
tawhidib <tawhidib@gmail.com>
2023-02-27 13:16:01 +0600

    Fix: class is replaced by className

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import Todo from "./Todo";
 
 function TodoList() {
   return (
-    <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+    <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {/* <!-- todo --> */}
       <Todo />
     </div>
